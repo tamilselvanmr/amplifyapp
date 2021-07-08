b0449f95952f7f115b6a70fec757ba705fe927f5
Tamilselvan <58145135+tamilselvanmr@users.noreply.github.com>
2021-07-09 02:24:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  <p>React app</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
